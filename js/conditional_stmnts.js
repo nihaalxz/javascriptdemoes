f3c85a75c828a6f459ba4f29e08fe49562c77cdc
nihaalxz <nihaal1010@gmail.com>
2024-12-12 12:21:59 +0530

    minor changes

---
+++ js/conditional_stmnts.js
@@ -0,0 +1,29 @@
+// if
+// if else
+// else if
+// nested if
+// else ladder
+// switch
+
+if(10<12)
+{
+    console.log("10 is less than 12");
+}
+
+
+if(100>200)
+{
+    console.log("100 is less than 200");
+}
+else
+{
+    console.log("100 is greater than 200");
+}
+ //voting system
+
+// 0-10 age show red color not eligible youre too young
+//11-17 your voting age is coming soon
+// age<0 invalid age
+// age>100 ypu need special permision
+
+//res.innerhtml=
